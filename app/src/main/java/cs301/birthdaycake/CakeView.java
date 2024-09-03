15d626b5a9581349c5b5ab39e61803874069cf9a
meganou <ou27@up.edu>
2024-09-03 14:14:42 -0700

    Add frosting slider and second candle

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -119,8 +119,11 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        //Now a candle to the left
+        drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/3, cakeTop);
+
+        //Draw a candle to the right
+        drawCandle(canvas, cakeLeft + (2 * cakeWidth/3) - (2 * candleWidth/3), cakeTop);
 
     }//onDraw
 
